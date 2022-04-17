70ee31cb26dc1b469e5966022570af1057e07cba
UTurtle <69633808+UTurtle@users.noreply.github.com>
2022-04-18 02:35:13 +0900

    - 전체적인 폰트 변경 - 가사(자막) 추출 부분 코드 변경 (248, 250, 304, 306) - 앨범 커버를 만들지 안만들지 옵션 추가 (364~378)

--- src/tkinter_pytube.py
+++ src/tkinter_pytube.py
@@ -35,7 +35,7 @@ def app():
 
     # Constant
     unknown = "unknown"
-    custom_font = 'Segoe ui'
+    default_font = "Segoe ui"
 
     # Variable
     is_audio = BooleanVar()
@@ -245,11 +245,11 @@ def app():
             def extractSubtitle():
                 """확장자가 .smi인 파일을 추출"""
 
-                caption = yt.captions['ko']
+                caption = yt.captions.get_by_language_code('ko')
                 if caption is None:
-                    caption = yt.captions['en']
+                    caption = yt.captions.get_by_language_code('en')
                     if caption is None:
-                        info(f"{yt.title}는 한글(또는 영어) 자막이 없습니다!")
+                        info(f"{yt.title}는 한글(또는 영어) 자막이 없습니다!", level="WARNING")
                         return -1
 
                 f = open(f"{yt.download_path}/{yt.title}.smi", "w", encoding="UTF-8")
@@ -284,7 +284,7 @@ def app():
 
             try:
                 video.download(f"{yt.download_path}")
-                info(f"{yt.title}를 다운로드 했습니다!\n\n")
+                info(f"{yt.title}를 다운로드 했습니다!\n\n", level="DONE")
             except FileExistsError:
                 print(f"이미 {yt.title}이(가) 있습니다!")
 
@@ -301,9 +301,9 @@ def app():
             def extractLyricsFile():
                 """확장자가 .lrc인 파일을 가져옴"""
 
-                caption = yt.captions['ko']
+                caption = yt.captions.get_by_language_code('ko')
                 if caption is None:
-                    caption = yt.captions['en']
+                    caption = yt.captions.get_by_language_code('en')
                     if caption is None:
                         info(f"{yt.title}는 한글(또는 영어) 자막이 없습니다!", level="WARNING")
                         return -1
@@ -361,13 +361,21 @@ def app():
                     urllib.request.urlretrieve(yt.thumbnail_url, mp3_thumbnail_path)
                     metadata = getMetaData()
                     sound = AudioSegment.from_mp3(audio_file)
-                    file_handle = sound.export(audio_file,
-                                               format="mp3",
-                                               tags={
-                                                   "album": f"{metadata.get('album')}",
-                                                   "artist": f"{metadata.get('artist')}"
-                                               },
-                                               cover=mp3_thumbnail_path)
+                    if set_cover.get():
+                        file_handle = sound.export(audio_file,
+                                                   format="mp3",
+                                                   tags={
+                                                       "album": f"{metadata.get('album')}",
+                                                       "artist": f"{metadata.get('artist')}"
+                                                   },
+                                                   cover=mp3_thumbnail_path)
+                    else:
+                        file_handle = sound.export(audio_file,
+                                                   format="mp3",
+                                                   tags={
+                                                       "album": f"{metadata.get('album')}",
+                                                       "artist": f"{metadata.get('artist')}"
+                                                   })
                     info(f"{yt.title}의 속성 추가를 완료했습니다!\n\n", level="DONE")
                     if os.path.isfile(mp3_thumbnail_path):
                         os.remove(mp3_thumbnail_path)
@@ -497,7 +505,7 @@ def app():
     # =========================================================
     title_label = Label(root)
     title_label["anchor"] = "center"
-    ft = tkFont.Font(family=custom_font, size=22, weight='bold')
+    ft = tkFont.Font(size=22, weight='bold')
     title_label["font"] = ft
     title_label["justify"] = "center"
     title_label["text"] = "Simple Youtube Downloader"
@@ -505,7 +513,7 @@ def app():
     title_label.place(x=0, y=0, width=600, height=50)
 
     file_save_path_label = Label(root)
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(size=10)
     file_save_path_label["font"] = ft
     file_save_path_label["justify"] = "left"
     file_save_path_label["text"] = "File Save Path"
@@ -517,7 +525,7 @@ def app():
     file_explore_button.place(x=50, y=80, width=100, height=30)
 
     file_save_path_combobox = tkinter.ttk.Combobox(root)
-    ft = tkFont.Font(family=custom_font, size=12)
+    ft = tkFont.Font( size=12)
     file_save_path_combobox["font"] = ft
     file_save_path_combobox["justify"] = "left"
     file_save_path_combobox["text"] = "File Save Path"
@@ -532,9 +540,9 @@ def app():
     logger_box = ScrolledText(root, state='disabled', height=12)
     logger_box.place(x=600, y=0, width=500, height=550)
 
-    ft = tkFont.Font(size=12)
-    ft_done = tkFont.Font(size=12, weight="bold")
-    ft_error = tkFont.Font(size=12, weight="bold")
+    ft = tkFont.Font(family=default_font, size=12)
+    ft_done = tkFont.Font(family=default_font, size=12, weight="bold")
+    ft_error = tkFont.Font(family=default_font, size=12, weight="bold")
     logger_box.configure(font=ft)
     logger_box.tag_config('INFO', foreground='black')
     logger_box.tag_config('DEBUG', foreground='gray')
@@ -549,7 +557,7 @@ def app():
     tabbar.add(single_download_page, text="Single Downloader")
 
     youtube_link_label = Label(single_download_page)
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     youtube_link_label["font"] = ft
     youtube_link_label["justify"] = "left"
     youtube_link_label["text"] = "Youtube Link를 넣어주세요 (is_play_list를 체크하면 재생목록을 다운받습니다.)"
@@ -557,7 +565,7 @@ def app():
 
     youtube_link_entry = Entry(single_download_page)
     youtube_link_entry["borderwidth"] = "1px"
-    ft = tkFont.Font(family=custom_font, size=12)
+    ft = tkFont.Font(family=default_font, size=12)
     youtube_link_entry["font"] = ft
     youtube_link_entry["justify"] = "left"
     youtube_link_entry["text"] = "Youtube Link"
@@ -566,7 +574,7 @@ def app():
     youtube_link_entry_url_paste_button = Button(single_download_page)
     youtube_link_entry_url_paste_button["anchor"] = "center"
     youtube_link_entry_url_paste_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     youtube_link_entry_url_paste_button["font"] = ft
     youtube_link_entry_url_paste_button["justify"] = "center"
     youtube_link_entry_url_paste_button["text"] = "Paste URL"
@@ -576,7 +584,7 @@ def app():
     # repeat
     is_audio_checkbutton = Checkbutton(single_download_page)
     is_audio_checkbutton["anchor"] = "w"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     is_audio_checkbutton["font"] = ft
     is_audio_checkbutton["justify"] = "left"
     is_audio_checkbutton["text"] = " is_audio?"
@@ -585,7 +593,7 @@ def app():
 
     is_playlist_checkbutton = Checkbutton(single_download_page)
     is_playlist_checkbutton["anchor"] = "w"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     is_playlist_checkbutton["font"] = ft
     is_playlist_checkbutton["justify"] = "left"
     is_playlist_checkbutton["text"] = " is_play_list?"
@@ -594,7 +602,7 @@ def app():
 
     open_folder_button = Button(single_download_page)
     open_folder_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     open_folder_button["font"] = ft
     open_folder_button["justify"] = "center"
     open_folder_button["text"] = "Open Folder"
@@ -604,7 +612,7 @@ def app():
     single_download_button = Button(single_download_page)
     single_download_button["anchor"] = "center"
     single_download_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10, weight="bold")
+    ft = tkFont.Font(family=default_font, size=10, weight="bold")
     single_download_button["font"] = ft
     single_download_button["justify"] = "center"
     single_download_button["text"] = "Download!"
@@ -618,7 +626,7 @@ def app():
     tabbar.add(multiple_download_page, text="Multiple Downloader")
 
     youtube_link_label = Label(multiple_download_page)
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     youtube_link_label["font"] = ft
     youtube_link_label["justify"] = "left"
     youtube_link_label["text"] = "유튜브 링크를 한줄씩 입력해 주세요"
@@ -626,14 +634,14 @@ def app():
 
     youtube_links_input_area = Text(multiple_download_page)
     youtube_links_input_area["borderwidth"] = "1px"
-    ft = tkFont.Font(family=custom_font, size=12)
+    ft = tkFont.Font(family=default_font, size=12)
     youtube_links_input_area["font"] = ft
     youtube_links_input_area.place(x=50, y=40, width=500, height=150)
 
     youtube_links_input_area_paste_url_button = Button(multiple_download_page)
     youtube_links_input_area_paste_url_button["anchor"] = "center"
     youtube_links_input_area_paste_url_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     youtube_links_input_area_paste_url_button["font"] = ft
     youtube_links_input_area_paste_url_button["justify"] = "center"
     youtube_links_input_area_paste_url_button["text"] = "Paste URL"
@@ -643,7 +651,7 @@ def app():
     # repeat
     is_audio_check_button = Checkbutton(multiple_download_page)
     is_audio_check_button["anchor"] = "w"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     is_audio_check_button["font"] = ft
     is_audio_check_button["justify"] = "left"
     is_audio_check_button["text"] = " is_audio?"
@@ -653,7 +661,7 @@ def app():
 
     is_playlist_check_button = Checkbutton(multiple_download_page)
     is_playlist_check_button["anchor"] = "w"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     is_playlist_check_button["font"] = ft
     is_playlist_check_button["justify"] = "left"
     is_playlist_check_button["text"] = " is_play_list?"
@@ -663,7 +671,7 @@ def app():
 
     open_folder_button = Button(multiple_download_page)
     open_folder_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10)
+    ft = tkFont.Font(family=default_font, size=10)
     open_folder_button["font"] = ft
     open_folder_button["justify"] = "center"
     open_folder_button["text"] = "Open Folder"
@@ -673,7 +681,7 @@ def app():
     multiple_download_button = Button(multiple_download_page)
     multiple_download_button["anchor"] = "center"
     multiple_download_button["bg"] = "#efefef"
-    ft = tkFont.Font(family=custom_font, size=10, weight="bold")
+    ft = tkFont.Font(family=default_font, size=10, weight="bold")
     multiple_download_button["font"] = ft
     multiple_download_button["justify"] = "center"
     multiple_download_button["text"] = "Download!"
